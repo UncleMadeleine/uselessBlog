fc5b94339e12f384f1b26e6f14526e0f47fa00e0
UncleMadeleine <59909003+UncleMadeleine@users.noreply.github.com>
2021-07-23 00:59:31 +0800

    test

--- python_file/run.py
+++ python_file/run.py
@@ -1,9 +1,10 @@
 import urllib.request
 import json
-
-
+import sys
+# import getpot
+wqy = sys.argv
 data = {
-  "ToUserUid":776223056,
+  "ToUserUid":int(wqy[1]),
   "SendToType":2,
   "SendMsgType":"TextMsg",
   "Content":"yzb又瞎push了什么怪东西"
@@ -14,5 +15,5 @@ print(data)
 print(values)
 print(json.dumps(data))
 print(json.dumps(data).encode())
-request = urllib.request.Request(url='http://121.40.124.19:8888/v1/LuaApiCaller?qq=1004619265&funcname=SendMsgV2&timeout=100', headers=headers, data=json.dumps(data).encode())
+request = urllib.request.Request(url=wqy[2], headers=headers, data=json.dumps(data).encode())
 response = urllib.request.urlopen(request)
