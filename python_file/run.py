d0719ed06af7253961f45d898620d22d7dedc4ad
UncleMadeleine <59909003+UncleMadeleine@users.noreply.github.com>
2021-07-24 11:36:46 +0800

    修复了一下

--- python_file/run.py
+++ python_file/run.py
@@ -8,21 +8,21 @@ with open('../commit.log', 'r') as f:
 	wcr = f.readlines()
 flag=1
 for i in wcr:
-	if flag:
-		continue
-	if len(i) < 3:
-		continue
 	if i[0:6] == "Author":
 		ljw = i.split("<")
 		i = ljw[0]
 		flag=0
+	if flag:
+    		continue
+	if len(i.strip()) < 3:
+		continue
 	str+=i.strip()
 f.close()
 data = {
 	"ToUserUid":int(wqy[1]),
 	"SendToType":2,
 	"SendMsgType":"TextMsg",
-	"Content": "yzb又瞎push了什么怪东西进https://github.com/UncleMadeleine/uselessBlog/commits/main："+str
+	"Content": "yzb又瞎push了什么怪东西进https://github.com/UncleMadeleine/uselessBlog/commits/main：\n"+str
 }
 values = urllib.parse.urlencode(data).encode(encoding='UTF8')
 headers = {'Content-Type': 'application/json'}
